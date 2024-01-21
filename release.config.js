581274e64925f7b54f61b8823e57ebc721af08a6
corinz97 <jacopocorina@live.it>
2024-01-21 21:12:21 +0100

    ci(rule): added gh packages publication

--- release.config.js
+++ release.config.js
@@ -1,6 +1,7 @@
 var publishCmd = `
 ./gradlew -PstagingRepositoryId=\${process.env.STAGING_REPO_ID} releaseStagingRepositoryOnMavenCentral || exit 3
 ./gradlew publishJsPackageToNpmjsRegistry || exit 4
+./gradlew publishAllPublicationsToGitHubRepository || exit 5
 `
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
